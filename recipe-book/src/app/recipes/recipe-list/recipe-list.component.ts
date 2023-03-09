306cce9229757ab731e467248c5e5f924eba8de2
Valentin Vadász <vadaszvalentin@gmail.com>
2023-03-09 12:18:24 +0100

    Shopping list - add ingredients

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,7 +7,13 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() selectRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
+    new Recipe('Babguláás', 'This is simply a test', 'https://post.healthline.com/wp-content/uploads/2018/04/steak-meat-1200x628-facebook-1200x628.jpg'),
   ];
+
+  onRecipeSelect(i: number) {
+    this.selectRecipe.emit(this.recipes[i]);
+  }
 }
--- recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @ViewChild('amountInput') amountInput: ElementRef;
+  onAddIngredient(name: string) {
+    this.ingredientAdded.emit(new Ingredient(name, this.amountInput.nativeElement.value));
+  }
 }
